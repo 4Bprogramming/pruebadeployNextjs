cac98834f129eec6a94bc1ffe1439ce071c25c3e
4Bprogramming <4b.programming@gmail.com>
2024-09-18 18:18:51 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      colors:{
+        primary: '#081F2D',
+        second: '#DF854F',
+        third: '#C8A977'
+      },
+      fontFamily:{
+        'lato':['lato','sans-serif']
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
